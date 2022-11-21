bddc03f34c20c362ca0113b886e35310843fc45d
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-11-21 17:24:04 -0500

    Completed assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Cmp1Component } from './components/cmp1/cmp1.component';
+import { Cmp2Component } from './components/cmp2/cmp2.component';
+import { Cmp3Component } from './components/cmp3/cmp3.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'cmp1', component: Cmp1Component},
+  {path: 'cmp2', component: Cmp2Component},
+  {path: 'cmp3', component: Cmp3Component},
+  {path: '**', redirectTo: '/'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Cmp1Component } from './components/cmp1/cmp1.component';
+import { Cmp2Component } from './components/cmp2/cmp2.component';
+import { Cmp3Component } from './components/cmp3/cmp3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Cmp1Component,
+    Cmp2Component,
+    Cmp3Component
   ],
   imports: [
     BrowserModule,
